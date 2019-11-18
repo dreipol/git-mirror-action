4dac54d16c59bb0bda14cb00d9fb17d9d9331563
Sidney Widmer <sidneywidmer@gmail.com>
2019-11-18 17:18:55 +0100

    Initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -eu
+
+# Setup ssh
+mkdir ~/.ssh
+echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+
+# Setup git
+export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -l $INPUT_USER"
+
+# Mirror it!
+git remote add mirror "$INPUT_REPO"
+git push mirror --force "$INPUT_SOURCE_BRANCH":"$INPUT_TARGET_BRANCH"
