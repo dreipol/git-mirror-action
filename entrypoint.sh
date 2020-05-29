ac8b87da657470eca6573dcecab15cef153c1ada
Sidney Widmer <sidneywidmer@gmail.com>
2020-05-29 13:12:51 +0200

    Add back --force

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,4 +12,4 @@ export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -l $INPUT_USER"
 # Mirror it!
 git remote add mirror "$INPUT_REPO"
 git fetch mirror
-git push mirror HEAD:"$INPUT_TARGET_BRANCH"
+git push mirror --force HEAD:"$INPUT_TARGET_BRANCH"
