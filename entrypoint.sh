7eed2996f3c6752ced8d98ee01687f6096e4fb5c
Sidney <hello@sidney.dev>
2022-03-10 14:57:40 +0100

    And set it directly in the entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,7 @@ chmod 600 ~/.ssh/id_rsa
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -l $INPUT_USER"
 
 # Mirror it!
+git config --global lfs.allowincompletepush true
 git remote add mirror "$INPUT_REPO"
 git fetch mirror
 git push mirror --force HEAD:"$INPUT_TARGET_BRANCH"
